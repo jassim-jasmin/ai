32bb721f193000410f894fa36bf8908bd2cb4803
mj <mohammedjassim.jasmin@gmail.com>
2020-05-11 10:49:19 +0530

    revamp

--- ml/ner/crf/train.py
+++ ml/ner/crf/train.py
@@ -2,7 +2,21 @@ import sklearn_crfsuite
 import joblib
 from os import sep
 from ml.ner.crf.text_process import build_crfsuit_dataset
-import joblib
+
+def get_crf(max_iterations=100, all_possible_transitions=True, c2=None, c1=None, algorithm='lbfgs'):
+    return sklearn_crfsuite.CRF(
+        algorithm=algorithm,
+        c1=c1,
+        c2=c2,
+        max_iterations=max_iterations,
+        all_possible_transitions=all_possible_transitions)
+
+def save_pickle(model, model_name):
+    try:
+        joblib.dump(model, f"model{sep}{model_name}.pkl")
+
+    except Exception as e:
+        print("error in save_pickle")
 
 def get_model(model_name):
     try:
@@ -16,16 +30,11 @@ def get_model(model_name):
 
 def train_model(X_train, Y_train, model_name):
     print("begin training")
-    crf = sklearn_crfsuite.CRF(
-        algorithm='lbfgs',
-        c1=0.1,
-        c2=0.1,
-        max_iterations=100,
-        all_possible_transitions=True)
+    crf = get_crf(c1=0.1, c2=0.1)
 
     crf.fit(X_train, Y_train)
 
-    joblib.dump(crf, f"model{sep}{model_name}.pkl")
+    save_pickle(crf, model_name)
     print(f"completed and saved model{sep}{model_name}.pkl")
 
     return crf

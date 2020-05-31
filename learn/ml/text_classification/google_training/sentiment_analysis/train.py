fb29783b1755045f51acc7fa0419032a000643ec
mj <mohammedjassim.jasmin@gmail.com>
2020-05-31 10:17:12 +0530

    missing file added

---
+++ learn/ml/text_classification/google_training/sentiment_analysis/train.py
@@ -0,0 +1,88 @@
+from learn.ml.text_classification.google_training.sentiment_analysis import vectorize_data, build_model, explore_data
+import tensorflow as tf
+
+
+def train_ngram_model(data,
+                      learning_rate=1e-3,
+                      epochs=1000,
+                      batch_size=128,
+                      layers=2,
+                      units=64,
+                      dropout_rate=0.2):
+    """Trains n-gram model on the given dataset.
+
+    # Arguments
+        data: tuples of training and test texts and labels.
+        learning_rate: float, learning rate for training model.
+        epochs: int, number of epochs.
+        batch_size: int, number of samples per batch.
+        layers: int, number of `Dense` layers in the model.
+        units: int, output dimension of Dense layers in the model.
+        dropout_rate: float: percentage of input to drop at Dropout layers.
+
+    # Raises
+        ValueError: If validation data has label values which were not seen
+            in the training data.
+    """
+    # Get the data.
+    (train_texts, train_labels), (val_texts, val_labels) = data
+
+    # Verify that validation labels are in the same range as training labels.
+    num_classes = explore_data.get_num_classes(train_labels)
+    unexpected_labels = [v for v in val_labels if v not in range(num_classes)]
+    if len(unexpected_labels):
+        raise ValueError('Unexpected label values found in the validation set:'
+                         ' {unexpected_labels}. Please make sure that the '
+                         'labels in the validation set are in the same range '
+                         'as training labels.'.format(
+                             unexpected_labels=unexpected_labels))
+
+    # Vectorize texts.
+    x_train, x_val = vectorize_data.ngram_vectorize(
+        train_texts, train_labels, val_texts)
+
+    # Create model instance.
+    model = build_model.mlp_model(layers=layers,
+                                  units=units,
+                                  dropout_rate=dropout_rate,
+                                  input_shape=x_train.shape[1:],
+                                  num_classes=num_classes)
+
+    # Compile model with learning parameters.
+    if num_classes == 2:
+        loss = 'binary_crossentropy'
+    else:
+        loss = 'sparse_categorical_crossentropy'
+    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
+
+    # Create callback for early stopping on validation loss. If the loss does
+    # not decrease in two consecutive tries, stop training.
+    callbacks = [tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=2)]
+
+    # Train and validate model.
+    history = model.fit(
+            x_train,
+            train_labels,
+            epochs=epochs,
+            callbacks=callbacks,
+            validation_data=(x_val, val_labels),
+            verbose=2,  # Logs once per epoch.
+            batch_size=batch_size)
+
+    # Print results.
+    history = history.history
+    print('Validation accuracy: {acc}, loss: {loss}'.format(
+            acc=history['val_acc'][-1], loss=history['val_loss'][-1]))
+
+    # Save model.
+    model.save('IMDb_mlp_model.h5')
+    return history['val_acc'][-1], history['val_loss'][-1]
+
+from learn.ml.text_classification.google_training.sentiment_analysis.get_dataset import load_imdb_sentiment_analysis_dataset, load_local_csv_dataset
+path_dataset = "C:\\Users\\MOHAMMED JASIM\\PycharmProjects\\ai\dataset\\nalaxone_arm_validation.csv"
+data_path = 'C:\\Users\\MOHAMMED JASIM\\PycharmProjects\\ai\dataset'
+data =load_local_csv_dataset(path_dataset)
+# data = load_imdb_sentiment_analysis_dataset(data_path)
+model = train_ngram_model(data)

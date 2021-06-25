277e246497dccb68dcbbc0d4fac5a2fe5eb507f8
Mohammed Jassim Jasmin <mohammedjassim.jasmin@gmail.com>
2021-06-25 09:03:50 +0530

    naive_bays_classifier comparison added

---
+++ other/learn/nlp/classifier/naive_bays_classifier.py
@@ -0,0 +1,28 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import precision_score, recall_score
+
+
+class NaiveBayesClassifier:
+    def __init__(self):
+        self.model = None
+
+    def get_model(self, train_vectors, test_vectors, y_train, embedding_name = None):
+        self.model = GaussianNB()
+
+        if embedding_name == 'tfidf':
+            self.model.fit(train_vectors.toarray(), y_train)
+        else:
+            self.model.fit(train_vectors, y_train)
+
+    def get_precision_and_recall(self, test_vectors, y_test, embedding_name=None):
+        if embedding_name == 'tfidf':
+            y_pred = self.model.predict(test_vectors.toarray())
+
+        else:
+            y_pred = self.model.predict(test_vectors)
+        precision = round(precision_score(y_test, y_pred), 3)
+        recall = round(recall_score(y_test, y_pred), 3)
+        accuracy = round((y_pred == y_test).sum() / len(y_pred), 3)
+        print(f"Naive Bayes, {embedding_name}, precision: {precision}, recall: {recall}, accuracy: {accuracy}")
+
+        return precision, recall, accuracy
--- other/learn/nlp/compare_model/compare_classifier_model.py
+++ other/learn/nlp/compare_model/compare_classifier_model.py
@@ -4,6 +4,7 @@ import numpy as np
 from other.learn.nlp.classifier.random_forest import RandomForestClassifierModel
 from other.learn.nlp.classifier.embeddings import Embeddings
 from other.learn.nlp.classifier.preprocessing import simple_preprocessing
+from other.learn.nlp.classifier.naive_bays_classifier import NaiveBayesClassifier
 
 
 def spam_classifier():
@@ -16,15 +17,63 @@ def spam_classifier():
     embeddings = Embeddings()
 
     all_embeddings = embeddings.get_all_embeddings(X_train, X_test)
+    precision_list = []
+    recall_list = []
+    accuracy_list = []
+    embeddings = []
 
     for name, values in all_embeddings.items():
+        embeddings.append(f"Random forest_{name}")
         train_vectors, test_vectors = values
         rfc_model = RandomForestClassifierModel.get_model(train_vectors, y_train)
 
         precision, recall, accuracy = RandomForestClassifierModel.get_precision_and_recall(rfc_model, test_vectors,
                                                                                            y_test)
         print(f"Random forest, {name}, precision: {precision}, recall: {recall}, accuracy: {accuracy}")
+        precision_list.append(precision)
+        recall_list.append(recall)
+        accuracy_list.append(accuracy)
+
+        naive_bayes = NaiveBayesClassifier()
+        naive_bayes.get_model(train_vectors, test_vectors, y_train, name)
+        precision, recall, accuracy = naive_bayes.get_precision_and_recall(test_vectors, y_test, name)
+        precision_list.append(precision)
+        recall_list.append(recall)
+        accuracy_list.append(accuracy)
+        embeddings.append(f"naive_bayes_{name}")
+
+    return precision_list, recall_list, accuracy_list, embeddings
+
+
+def draw_graph(precision_list, recall_list, accuracy_list, embedding):
+    import matplotlib.pyplot as plt
+    # set width of bar
+    barWidth = 0.25
+    fig = plt.subplots(figsize=(12, 8))
+
+    # Set position of bar on X axis
+    br1 = np.arange(len(precision_list))
+    br2 = [x + barWidth for x in br1]
+    br3 = [x + barWidth for x in br2]
+
+    # Make the plot
+    plt.bar(br1, precision_list, color='r', width=barWidth,
+            edgecolor='grey', label='precision')
+    plt.bar(br2, recall_list, color='g', width=barWidth,
+            edgecolor='grey', label='recall')
+    plt.bar(br3, accuracy_list, color='b', width=barWidth,
+            edgecolor='grey', label='accuracy')
+
+    # Adding Xticks
+    plt.xlabel('Model', fontweight='bold', fontsize=15)
+    plt.ylabel('Values', fontweight='bold', fontsize=15)
+    plt.xticks([r + barWidth for r in range(len(precision_list))],
+               embedding)
+
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    spam_classifier()
+    precision_list, recall_list, accuracy_list, embedding = spam_classifier()
+    draw_graph(precision_list, recall_list, accuracy_list, embedding)
